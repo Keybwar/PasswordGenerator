90542596672a04c12ed67267b76b54c93442b555
Keybwar <107952779+Keybwar@users.noreply.github.com>
2023-09-06 13:26:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "?","=",".","*"];
+
+const charactersOdd = ["$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/","~","`","!","@","#"]
+
+let passwordEl = document.querySelector("#password-el")
+let passwordEl1 = document.querySelector("#password-el1")
+
+function genPass(){
+    for (let i = 0; i < characters.length; i++){
+        let randomIndex = Math.floor(Math.random()*characters.length)
+        let randomIndex1 = Math.floor(Math.random()*characters.length)
+        let randomIndex2 = Math.floor(Math.random()*characters.length)
+        let randomIndex3 = Math.floor(Math.random()*characters.length)
+        let randomIndex4 = Math.floor(Math.random()*characters.length)
+        let randomIndex5 = Math.floor(Math.random()*characters.length)
+        let randomIndex6 = Math.floor(Math.random()*characters.length)
+        let randomIndex7 = Math.floor(Math.random()*characters.length)
+        let randomIndex8 = Math.floor(Math.random()*characters.length)
+        let randomIndex9 = Math.floor(Math.random()*characters.length)
+        let randomIndex10 = Math.floor(Math.random()*characters.length)
+        let randomIndex11 = Math.floor(Math.random()*characters.length)
+        passwordEl.textContent = characters[randomIndex] + characters[randomIndex1] + characters[randomIndex2] + characters[randomIndex3] + characters[randomIndex4] + characters[randomIndex5] + characters[randomIndex6] + characters[randomIndex7] + characters[randomIndex8] + characters[randomIndex9] + characters[randomIndex10] + characters[randomIndex11]
+        
+        let savePass = characters[randomIndex] + characters[randomIndex1] + characters[randomIndex2] + characters[randomIndex3] + characters[randomIndex4] + characters[randomIndex5] + characters[randomIndex6] + characters[randomIndex7] + characters[randomIndex8] + characters[randomIndex9] + characters[randomIndex10] + characters[randomIndex11]
+        return savePass
+    }
+    console.log("click")
+    
+}
+
+function copyPassword(event) {
+    
+    let selectedEl = document.getElementById(event.target.id);
+
+    if (selectedEl.textContent !== "") {
+        navigator.clipboard.writeText(selectedEl.textContent);
+    }
+}
